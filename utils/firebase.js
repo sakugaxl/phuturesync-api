922e3cfbe22ed5016ba0c0b4c2826139c0fb025f
sakugaxl <bekiwe01@gmail.com>
2024-12-11 01:17:33 +0200

    Firebase Fixes

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,5 +1,15 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../serviceAccountKey.json");
+
+// Determine service account credentials dynamically
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT_KEY) {
+  // Use environment variable in production
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+} else {
+  // Use local JSON file for development
+  serviceAccount = require("../serviceAccountKey.json");
+}
 
 if (!admin.apps.length) {
   admin.initializeApp({
