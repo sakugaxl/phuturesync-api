d1fe649632034598b5f648f47f612304290ce626
sakugaxl <bekiwe01@gmail.com>
2024-12-15 19:21:47 +0200

    API Panel v1.0.7 (Base64 Parsing System)

--- utils/firebaseAdmin.js
+++ utils/firebaseAdmin.js
@@ -2,13 +2,16 @@
 const admin = require('firebase-admin');
 
 if (!admin.apps.length) {
+  const decodedKey = Buffer.from(process.env.FIREBASE_PRIVATE_KEY_BASE64, 'base64').toString('utf-8');
+  
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      privateKey: decodedKey,
     }),
   });
 }
+
 const db = admin.firestore();
 module.exports = { db };
