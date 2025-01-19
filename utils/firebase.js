a0cfb995c1e00745096f16b82bf18f657837a1d7
sakugaxl <bekiwe01@gmail.com>
2025-01-19 03:30:07 +0200

    Add utility functions for backend

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,37 +1,12 @@
-// utils/firebase.js
-const { initializeApp } = require("firebase/app");
-const { getFirestore } = require("firebase/firestore");
-const { errorHandler } = require("./helpers");
+const admin = require("firebase-admin");
+const serviceAccount = require("../serviceAccountKey.json");
 
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGE_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-};
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
 
-let app;
-let firestoreDb;
+const db = admin.firestore();
 
-const initializeFirebaseApp = () => {
-  try {
-    if (!app) {
-      app = initializeApp(firebaseConfig);
-      firestoreDb = getFirestore();
-    }
-  } catch (error) {
-    errorHandler(error, "firebase-initializeFirebaseApp");
-  }
-};
-
-const getFirebaseApp = () => app;
-const getFirestoreDb = () => firestoreDb;
-
-module.exports = {
-  initializeFirebaseApp,
-  getFirebaseApp,
-  getFirestoreDb,
-};
+module.exports = { db };
